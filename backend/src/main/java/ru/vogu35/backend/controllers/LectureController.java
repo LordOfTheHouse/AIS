78fb34e59095fc66c6700b962e47c04a432d0fe8
katerniuksm <ke.s2013@yandex.ru>
2024-01-18 19:22:14 +0300

    add lecture method

--- backend/src/main/java/ru/vogu35/backend/controllers/LectureController.java
+++ backend/src/main/java/ru/vogu35/backend/controllers/LectureController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import ru.vogu35.backend.models.GroupModel;
 import ru.vogu35.backend.models.UserInfo;
 import ru.vogu35.backend.models.UserResponse;
 import ru.vogu35.backend.proxies.KeycloakApiProxy;
+import ru.vogu35.backend.repositories.GroupRepository;
 
 import java.util.List;
 
@@ -18,13 +20,19 @@ import java.util.List;
 public class LectureController {
 
     private final KeycloakApiProxy keycloakApiProxy;
+    private final GroupRepository groupRepository;
 
-    @Autowired
-    public LectureController(KeycloakApiProxy keycloakApiProxy) {
+    public LectureController(KeycloakApiProxy keycloakApiProxy, GroupRepository groupRepository) {
         this.keycloakApiProxy = keycloakApiProxy;
+        this.groupRepository = groupRepository;
     }
 
+    @PreAuthorize("hasRole('client_teacher')")
+    @GetMapping("/groups")
+    public ResponseEntity<List<GroupModel>> getGroups(){
 
+        return ResponseEntity.ok(null);
+    }
     @GetMapping("/{group}")
     public ResponseEntity<List<UserResponse>> getStudentGroup(@PathVariable("group") String groupName){
         log.info("students group: {}", groupName);
